3f6526b658042c03914b9da467d41da8f5af6038
9Nassim <131931654+9Nassim@users.noreply.github.com>
2025-05-14 09:59:18 +0200

    changements fait par sefraoui pour la phase 3
    ajout du fichier trek.css

--- trek-algérie.php
+++ trek-algérie.php
@@ -56,7 +56,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Trek Algérie | Rajjel Agency</title>
-
+    <link rel="stylesheet" href="trek.css">
 </head>
 
 <body>
